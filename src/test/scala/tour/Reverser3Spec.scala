cd54543ab9f03e007f3179aab9ee556d9a8ec8b0
etorreborre <etorreborre@yahoo.com>
2010-05-18 09:45:03 +1000

    added a scalacheck spec

---
+++ src/test/scala/tour/Reverser3Spec.scala
@@ -0,0 +1,25 @@
+package tour
+import org.specs._
+import IncredibleStringReverser._
+import org.scalacheck._
+import org.scalacheck.Prop.classify
+
+class Reverser3Spec extends SpecificationWithJUnit with ScalaCheck with Sugar {
+  "reverse must preserve the length of a string" verifies { s: String =>	 
+    reverse(s).size == s.size 
+  }
+  "reverse applied twice must return the original string" verifies { s: String =>	 
+    reverse(reverse(s)) == s 
+  }
+  "reverse 2 concatenated strings must return the reversed second string\n" + 
+  "concatenated with the reverse of the first one" verifies { (s1: String, s2: String) =>	 
+    reverse(s1 + s2) == reverse(s2) + reverse(s1) 
+  }
+  def center(s: String): Char = s(s.size / 2)
+  implicit def simpleStrings: Arbitrary[String] = Arbitrary {
+    for { l <- Gen.listOfN(4, Gen.oneOf('a', 'b', 'c')) } yield l.mkString
+  }
+  "keep the same 'center' character - Woops!" verifies { s: String =>
+    s.isEmpty || (center(s.reverse) == center(s)) 
+  }.display(minTestsOk -> 5)
+}
